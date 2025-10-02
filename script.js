871be9cf3ab2e47e9a9fb622d9c02fd7a9768948
bayufadayan <bayufadayan@gmail.com>
2025-10-02 22:38:48 +0700

    style: responsive

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-window.addEventListener('scroll', function () {
-    const sky = document.querySelector('.sky-background');
-    const title = document.querySelector('.hero-title');
-    const trigger = window.innerHeight * 0.8;
+// window.addEventListener('scroll', function () {
+//     const sky = document.querySelector('.sky-background');
+//     const title = document.querySelector('.hero-title');
+//     const trigger = window.innerHeight * 0.8;
 
-    if (window.scrollY >= trigger) {
-        sky.style.position = 'absolute';
-        title.style.position = 'absolute';
-        title.style.top = '35vh';
-    } else {
-        sky.style.position = 'fixed';
-        title.style.position = 'fixed';
-        title.style.top = '35vh';
-    }
-});
+//     if (window.scrollY >= trigger) {
+//         sky.style.position = 'absolute';
+//         title.style.position = 'absolute';
+//         title.style.top = '35vh';
+//     } else {
+//         sky.style.position = 'fixed';
+//         title.style.position = 'fixed';
+//         title.style.top = '35vh';
+//     }
+// });
 
 const dropdown = document.getElementById('dropdown');
 const selected = dropdown.querySelector('.selected');
