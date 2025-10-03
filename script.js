7b2f20461d179c6f941cb1c416c182aee3b489e0
bayufadayan <bayufadayan@gmail.com>
2025-10-03 11:20:07 +0700

    feat: add animation

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-// window.addEventListener('scroll', function () {
-//     const sky = document.querySelector('.sky-background');
-//     const title = document.querySelector('.hero-title');
-//     const trigger = window.innerHeight * 0.8;
+const isMobile = window.innerWidth < 685;
 
-//     if (window.scrollY >= trigger) {
-//         sky.style.position = 'absolute';
-//         title.style.position = 'absolute';
-//         title.style.top = '35vh';
-//     } else {
-//         sky.style.position = 'fixed';
-//         title.style.position = 'fixed';
-//         title.style.top = '35vh';
-//     }
-// });
+window.addEventListener('scroll', function () {
+    const sky = document.querySelector('.sky-background');
+    const title = document.querySelector('.hero-title');
+    const triggerDesktop = window.innerHeight * 0.8;
+    const triggerMobile = window.innerHeight * 0.48;
+
+    if (isMobile) {
+        sky.style.position = 'absolute';
+        if (window.scrollY >= triggerMobile) {
+            sky.style.position = 'absolute';
+            title.style.position = 'absolute';
+        } else {
+            title.style.position = 'fixed';
+        }
+    } else {
+        if (window.scrollY >= triggerDesktop) {
+            sky.style.position = 'absolute';
+            title.style.position = 'absolute';
+        } else {
+            sky.style.position = 'fixed';
+            title.style.position = 'fixed';
+        }
+    }
+});
 
 const dropdown = document.getElementById('dropdown');
 const selected = dropdown.querySelector('.selected');
@@ -30,3 +41,59 @@ list.querySelectorAll('li').forEach(item => {
         list.style.display = 'none';
     });
 });
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    const isScrolled = window.scrollY > 400;
+    if (isScrolled) {
+        header.style.backgroundColor = 'rgba(0, 0, 0, 0.75)';
+        header.style.paddingBlock = '1em';
+        if (isMobile) {
+            header.style.paddingBlock = '0.5rem';
+        }
+    } else {
+        header.style.backgroundColor = 'transparent';
+        header.style.padding = '56px';
+        if (isMobile) {
+            header.style.paddingBlock = '1em';
+        }
+    }
+});
+
+if (isMobile) {
+    document.getElementById("first-polaroid").classList.add("show-polaroid-1");
+    document.getElementById("second-polaroid").classList.add("show-polaroid-2");
+    document.getElementById("third-polaroid").classList.add("show-polaroid-3");
+    document.getElementById("fourth-polaroid").classList.add("show-polaroid-4");
+} else {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 200) {
+            document.getElementById("first-polaroid").classList.add("show-polaroid-1");
+            document.getElementById("second-polaroid").classList.add("show-polaroid-2");
+            document.getElementById("third-polaroid").classList.add("show-polaroid-3");
+            document.getElementById("fourth-polaroid").classList.add("show-polaroid-4");
+        }
+    });
+}
+
+window.addEventListener("scroll", () => {
+    const emperorTitle = document.querySelector(".emperor-title");
+
+    if (window.scrollY > window.innerHeight * 0.2) {
+        emperorTitle.classList.add("fade-in");
+    }
+});
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click", () => {
+    if (hamburger.classList.contains("open")) {
+        hamburger.classList.remove("open");
+        nav.style.display = "none";
+    } else {
+        hamburger.classList.add("open");
+        nav.style.display = "flex";
+    }
+});
+
